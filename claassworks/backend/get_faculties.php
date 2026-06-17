c9522f2395c6608dfab4a5de1a7f4b31ef48aa39
ShihoTL <mgbekenjustice2@gmail.com>
2026-06-17 12:33:17 +0100

    Merge

--- claassworks/backend/get_faculties.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$faculties = [
-    ["id" => 1, "name" => "Faculty of Computing"],
-    ["id" => 2, "name" => "Faculty of Engineering"]
-];
-
-header("Content-Type: application/json");
-echo json_encode($faculties);
--- claassworks/dbclass/model.php
+++
@@ -1,5 +0,0 @@
-<?php
-"include"
-function fetchAllCountries(){
-
-}
