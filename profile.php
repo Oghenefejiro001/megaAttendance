0f992a893a75904f21025d4723a9a4c132b4ee99
ShihoTL <mgbekenjustice2@gmail.com>
2026-07-02 12:08:16 +0100

    Added the assignment class

--- profile.php
+++ profile.php
@@ -1,12 +1,5 @@
 <?php session_start(); ?>
 
-<<<<<<< HEAD
-<?php if (!$_SESSION['authenticated']) {
-    header("location:register.php");
-    return;
-}
-    ?>
-=======
 <?php
 if (!$_SESSION['authenticated']) {
     echo "Access denied, please register";
@@ -16,7 +9,6 @@ if (!$_SESSION['authenticated']) {
 
 
 ?>
->>>>>>> 428b91999bafc9deeedcc25eaae0d87e6df6d360
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,19 +16,10 @@ if (!$_SESSION['authenticated']) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<<<<<<< HEAD
-
-<body>
-    <h1>Profile</h1>
-    <p> <?php echo $_SESSION['name']; ?>, welcome to our app! </p>
-    <p> Your Email is <?php echo $_SESSION['email']; ?> </p>
-    <button><a href="logout.php">Logout</a></button>
-=======
 <body>
     <h1>Profile</h1>
     <p><?=  $_SESSION['name']; ?>, Welcome to our app</p>
     <p>Your email is <?= $_SESSION['email'] ?></p>
    <a href="logout.php"> <button>Logout</button></a>
->>>>>>> 428b91999bafc9deeedcc25eaae0d87e6df6d360
 </body>
 </html>
